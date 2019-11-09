a17f3a61d88d7886c7addaf20ed0bd77b664b66e
xMustang <xMustang@foxmail.com>
2019-11-09 09:15:13 +0800

    ApplicationContext

--- src/test/java/com/xmustang/BeanFactoryTest.java
+++ src/test/java/com/xmustang/BeanFactoryTest.java
@@ -1,7 +1,10 @@
 package com.xmustang;
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 /**
@@ -9,9 +12,12 @@ import org.springframework.core.io.ClassPathResource;
  * description:
  */
 public class BeanFactoryTest {
-	@Test
-	public void testSimpleLoad() {
-		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
-		MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
-	}
+    @Test
+    public void testSimpleLoad() {
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
+
+        ApplicationContext ac = new ClassPathXmlApplicationContext("beanFactoryTest.xml");
+        ac.getBean("myTestBean");
+    }
 }
