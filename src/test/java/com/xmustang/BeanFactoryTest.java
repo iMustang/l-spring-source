5a4fefd1ab764d17a3d1738c2f883ef13faacf7d
宿城 <sucheng-1@thunisoft.com>
2019-11-12 18:12:06 +0800

    源码版本

--- src/test/java/com/xmustang/BeanFactoryTest.java
+++ src/test/java/com/xmustang/BeanFactoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.core.io.ClassPathResource;
 public class BeanFactoryTest {
     @Test
     public void testSimpleLoad() {
+		// 使用Spring 3.0.7.RELEASE版本研究
         BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
         MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
 
